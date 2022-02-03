0cae3e4c468cb812b0b0cc91178bcd2815a7970d
Mertinn <marcin.rajs@wp.pl>
2022-02-03 10:38:48 +0100

    Added custom marker icon

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import { StyledMapContainer } from "./styles";
+import MarkerIconSVG from "../../assets/icon-location.svg";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+const MarkerIcon = L.icon({
+  iconUrl: MarkerIconSVG,
+  iconSize: [35, 42],
+});
 
 const Map = () => {
   return (
@@ -12,7 +19,7 @@ const Map = () => {
         style={{ height: "100%" }}
       >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-        <Marker position={[51.505, -0.09]} />
+        <Marker position={[51.505, -0.09]} icon={MarkerIcon} />
       </MapContainer>
     </StyledMapContainer>
   );
